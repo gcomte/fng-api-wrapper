0ecefb06781275b1d4b7003b56965d4811acd986
Gabriel Comte <gabriel.comte@gmail.com>
2023-04-15 21:58:26 +0200

    Show API response on failing integration test

--- tests/int_tests.rs
+++ tests/int_tests.rs
@@ -2,6 +2,16 @@ use fng_api_wrapper::client;
 
 #[test]
 fn fetch_10_last_days() {
+    // For debugging reasons: If the test fails, the API response is printed.
+    println!(
+        "{}",
+        ureq::get("https://api.alternative.me/fng/?limit=10")
+            .call()
+            .unwrap()
+            .into_string()
+            .unwrap()
+    );
+
     let fng = client::fetch_daily_fng(10).unwrap();
     assert_eq!(fng.data.len(), 10);
 }
